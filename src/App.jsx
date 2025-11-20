cfeed4dac593534fff0d7167d571b0e6d6986c12
Aastha Dixit <dixitaastha13@gmail.com>
2025-11-20 16:17:20 +0530

    Implemented Add Book Page: form to add new books with validation, Redux state update, and redirect to Browse Books with latest book shown first

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import BrowseBooks from "./pages/BrowseBooks";
+import BookDetails from "./pages/BookDetails";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<><NavBar /><Home /></>} />
       <Route path="/books" element={<><NavBar /><BrowseBooks /></>} />
         <Route path="/books/:category" element={<><NavBar /><BrowseBooks /></>} />
+        <Route path="/book/:id" element={<><NavBar /><BookDetails /></>} />
       
     </div>
   )
