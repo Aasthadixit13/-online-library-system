0ea469524f5c14bd10df5f8cdb36aa52b8c98e9c
Aastha Dixit <dixitaastha13@gmail.com>
2025-11-20 15:57:49 +0530

    Added complete Home page: welcome message, responsive book categories (All, Fiction, Non-Fiction, Sci-Fi), popular books cards with View Details routing + integrated Navbar navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div>
       <h1>libary system</h1>
+      <Route path="/" element={<><NavBar /><Home /></>} />
       <Route path="/books" element={<><NavBar /><BrowseBooks /></>} />
     </div>
   )
